f04a6ef1a46616f44a217baa5f075784a8eeecb5
Paul Mackenzie <paul@whatspauldoing.com>
2025-09-04 02:04:36 +0100

    Creating Logo partial

--- src/components/dad-reply/Header.svelte
+++ src/components/dad-reply/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Logo from "./partials/Logo.svelte";
   import Badge from "./partials/Badge.svelte";
 </script>
 
@@ -6,37 +7,32 @@
   class="flex flex-row bg-white text-slate-700 items-center justify-between py-10 mb-4"
 >
   <div class="flex flex-row space-x-6">
-    <a href="#top">
-      <h2
-        class="flex items-center text-xl font-display font-bold tracking-tight"
-      >
-        <span class="mr-2">👍</span>
-        <span class="leading-none">Dad Reply</span>
-      </h2></a
-    >
+    <Logo />
     <div class="inline-flex items-center">
       <Badge text={"v1.0.0"} />
     </div>
   </div>
-  <ul class="flex flex-row space-x-3 text-xs font-medium [&_a]:hover:underline">
-    <li>
-      <a
-        href="https://www.producthunt.com/products/dad-reply"
-        target="_blank">Product Hunt</a
-      >
-    </li>
-    <li>
-      <a
-        href="https://www.linkedin.com/company/dadreply"
-        target="_blank">LinkedIn</a
-      >
-    </li>
-    <li class="text-slate-300">|</li>
-    <li>
-      <a
-        href="/"
-        class="text-blue-600">Claim early bird offer</a
-      >
-    </li>
-  </ul>
+  <nav class="text-xs font-medium [&_a]:hover:underline">
+    <ul class="flex flex-row space-x-3">
+      <li>
+        <a
+          href="https://www.producthunt.com/products/dad-reply"
+          target="_blank">Product Hunt</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/company/dadreply"
+          target="_blank">LinkedIn</a
+        >
+      </li>
+      <li class="text-slate-300">|</li>
+      <li>
+        <a
+          href="/"
+          class="text-blue-600">Claim early bird offer</a
+        >
+      </li>
+    </ul>
+  </nav>
 </header>
---
+++ src/components/dad-reply/partials/Logo.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<a href="#top">
+  <h2 class="flex items-center text-xl font-display font-bold tracking-tight">
+    <span class="mr-2">👍</span>
+    <span class="leading-none">Dad Reply</span>
+  </h2></a
+>
