8535358049a7514fc112c8e9973a340b73e270a4
Paul Mackenzie <paul@whatspauldoing.com>
2025-09-04 22:13:29 +0100

    Starting the 1.5.0 features list. Updating the hero email with buttons. Creating filepath references.

---
+++ src/components/dad-reply/ComingSoon.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-row gap-32 [&_p]:leading-relaxed items-start">
+  <div class="w-2/5 space-y-3.5 my-14 sticky top-1/3">
+    <h2 class="text-[32px] font-semibold">What to expect in v1.5.0</h2>
+    <p class="text-slate-500">
+      This dad’s hit the gym listened to the latest alpha male podcast* and is
+      ready to get back on the market*.
+    </p>
+    <small class="text-slate-400 text-xs">
+      * ignored the advice because its’s ridiculous nonsense.
+    </small>
+  </div>
+  <div class="flex-1 space-y-40">
+    <div class="space-y-6">
+      <h3 class="text-2xl font-semibold">Choose a tone that matches you.</h3>
+      <div class="border border-slate-300 rounded-lg p-6"></div>
+      <p class="text-slate-500">
+        In v1.5.0 you’ll have the ability to change the tone of thumb up. You
+        have the choice everywhere else, so why not here too.*
+      </p>
+      <small class="text-slate-400 text-xs">
+        * I understand that this is like selling a car on it having electric
+        windows.
+      </small>
+    </div>
+    <div class="space-y-6">
+      <h3 class="text-2xl font-semibold">Another button?</h3>
+      <div class="border border-slate-300 rounded-lg p-6"></div>
+      <p class="text-slate-500">
+        That right baby. This isn’t like selling electric windows in a new car.
+        This is like selling a purpose build napping pod.
+      </p>
+    </div>
+    <div class="space-y-6">
+      <h3 class="text-2xl font-semibold">Custom responses!</h3>
+      <div class="border border-slate-300 rounded-lg p-6"></div>
+      <p class="text-slate-500">
+        That right baby. This isn’t like selling electric windows in a new car.
+        This is like selling a purpose build napping pod. Check out this new
+        addition.
+      </p>
+    </div>
+  </div>
+</div>
+
+<style>
+  @reference "../../styles/dad-reply.css";
+  p {
+    @apply leading-relaxed text-slate-500;
+  }
+</style>
--- src/components/dad-reply/partials/DownloadButton.svelte
+++ src/components/dad-reply/partials/DownloadButton.svelte
@@ -10,7 +10,7 @@
 
 <a
   href={downloadLink}
-  class="flex text-sm p-3 border-2 rounded-lg font-semibold {bgColor} {borderColor} hover:{bgColorHover} {textColor} transition-colors duration-300 ease-in-out"
+  class="flex text-sm p-3 border-2 rounded-lg font-semibold {textColor} {bgColor} {borderColor} hover:{bgColorHover} transition-colors duration-300 ease-in-out"
 >
   <i class="fa-brands {'fa-' + icon} text-xl mr-2.5"></i>
   {"Download for " + platform}
