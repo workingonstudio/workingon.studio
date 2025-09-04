33545883ad08d5be59474107dedc884fe05b8371
Paul Mackenzie <paul@whatspauldoing.com>
2025-09-04 02:01:09 +0100

    Creating dad-reply layouts, pages, and partials. Bringing in required typefaces (including material icons)

---
+++ src/components/dad-reply/FeaturePills.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Pill from "./partials/Pill.svelte";
+</script>
+
+<ul
+  class="flex flex-row font-semibold text-xs/6 space-x-6 *:flex *:items-center *:border *:rounded-full *:border-slate-300 *:py-1 *:px-3 [&_span]:!text-[18px] [&_span]:mr-2 [&_span]:text-blue-600"
+>
+  <Pill
+    icon="add_circle"
+    text="Added directly in Gmail"
+  />
+  <Pill
+    icon="web_traffic"
+    text="Single click reply"
+  />
+  <Pill
+    icon="flash_off"
+    text="Not powered by AI"
+  />
+</ul>
---
+++ src/components/dad-reply/partials/Pill.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let icon: string;
+  export let text: string;
+</script>
+
+<li><span class="material-symbols-rounded">{icon}</span>{text}</li>
