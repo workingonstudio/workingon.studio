88e8175405329ad7e45d80fc508a57fe289a230f
Paul Mackenzie <paul@whatspauldoing.com>
2025-09-04 02:33:51 +0100

    Creating download button component. Creating email component

---
+++ src/components/dad-reply/Hero.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import FeaturePills from "./FeaturePills.svelte";
+  import DownloadButton from "./partials/DownloadButton.svelte";
+  import Email from "./partials/Email.svelte";
+</script>
+
+<section class="flex gap-2 items-center">
+  <div class="w-[684px] space-y-12">
+    <h1 class="text-[40px] font-semibold">
+      Instantly reply to your emails with minimal effort and maximum dad energy.
+    </h1>
+    <div class="w-[566px] text-slate-500 space-y-6">
+      <p>
+        Whether it’s that weekly status report, someone asking a question that
+        is already in the wiki, or a simple request for coffee. There’s now a
+        better way to respond to the mundane, inane or frivolous emails.
+      </p>
+      <FeaturePills />
+    </div>
+    <div class="flex gap-6 text-sm *:p-3 *:border-2 *:rounded-lg *:flex">
+      <DownloadButton />
+      <DownloadButton
+        platform="Edge"
+        icon="edge"
+        borderColor="border-blue-600"
+        bgColor="white"
+        bgColorHover="white"
+        textColor="text-blue-600"
+      />
+    </div>
+  </div>
+  <div
+    class="flex-1 bg-white border border-slate-300 rounded-[14px] p-16 the-shadow"
+  >
+    <Email />
+  </div>
+</section>
---
+++ src/components/dad-reply/partials/DownloadButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let platform: string = "Chrome";
+  export let textColor: string = "text-white";
+  export let bgColor: string = "bg-blue-600";
+  export let bgColorHover: string = "bg-blue-700";
+  export let borderColor: string = "border-transparent";
+  export let icon: string = "chrome";
+  export let downloadLink: string = "/";
+  export let clickable: boolean = true;
+</script>
+
+<a
+  href={downloadLink}
+  class="flex text-sm p-3 border-2 rounded-lg {bgColor} {borderColor} hover:{bgColorHover} transition-colors delay-100 {textColor} font-semibold"
+>
+  <i class="fa-brands {'fa-' + icon} text-xl mr-2.5"></i>
+  {"Download for " + platform}
+</a>
