7d54f7c3b852654faa4b59f55e62103f6c4ad202
Paul Mackenzie <paul@whatspauldoing.com>
2025-09-05 13:27:20 +0100

    Converting to Iconify web components.

--- src/components/dad-reply/partials/Email.svelte
+++ src/components/dad-reply/partials/Email.svelte
@@ -2,53 +2,41 @@
   import MailButton from "./MailButton.svelte";
 </script>
 
-<div class="flex flex-col space-y-8 text-black overflow-hidden">
+<div class="flex flex-col space-y-8 overflow-hidden text-black">
   <div class="space-y-5">
     <div class="font-[Roboto]">
       <strong class="text-[15px]">
         workingon.studio <span class="text-xs font-normal text-slate-400">
-          &lt;<a href="mailto:hello@workingon.studio">hello@workingon.studio</a
-          >&gt;
+          &lt;
+          <a href="mailto:hello@workingon.studio">hello@workingon.studio</a>
+          &gt;
         </span>
       </strong>
       <span class="flex items-center text-xs font-normal text-slate-400">
-        to you <span class="material-symbols-rounded !text-lg"
-          >arrow_drop_down</span
-        >
+        to you <span class="material-symbols-rounded !text-lg">arrow_drop_down</span>
       </span>
     </div>
-    <div class="font-[Arial] text-sm/7 space-y-5">
+    <div class="space-y-5 font-[Arial] text-sm/7">
       <p>To all,</p>
+      <p>Look, I'm tired of products that pretend to solve problems while creating new ones.</p>
       <p>
-        Look, I'm tired of products that pretend to solve problems while
-        creating new ones.
+        Tired of "user experiences" designed to extract rather than delight. Tired of the gap between what we say we're
+        building and what we're actually building.
       </p>
       <p>
-        Tired of "user experiences" designed to extract rather than delight.
-        Tired of the gap between what we say we're building and what we're
-        actually building.
+        So I'm building cultural mirror products. Things that hold up what people are unconsciously doing and make them
+        suddenly, uncomfortably aware of it.
       </p>
       <p>
-        So I'm building cultural mirror products. Things that hold up what
-        people are unconsciously doing and make them suddenly, uncomfortably
-        aware of it.
-      </p>
-      <p>
-        Not because I'm trying to fix broken systems. Because I'm meeting people
-        where they are and changing how they interact with those systems.
+        Not because I'm trying to fix broken systems. Because I'm meeting people where they are and changing how they
+        interact with those systems.
       </p>
       <p>Cheers.</p>
     </div>
   </div>
   <div class="flex flex-row space-x-3">
     <MailButton text="Dad Reply" />
-    <MailButton
-      text="Reply"
-      icon="reply"
-    />
-    <MailButton
-      text="Forward"
-      icon="forward"
-    />
+    <MailButton text="Reply" icon="reply-rounded" />
+    <MailButton text="Forward" icon="forward-rounded" />
   </div>
 </div>
