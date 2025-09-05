bae7844b30cb539e48006f69066a6daffd1ae848
Paul Mackenzie <paul@whatspauldoing.com>
2025-09-05 10:37:35 +0100

    Wiring up the tone selection radio buttons.

--- src/components/dad-reply/partials/Badge.svelte
+++ src/components/dad-reply/partials/Badge.svelte
@@ -3,7 +3,6 @@
   export let bgColor: string = "bg-slate-200";
 </script>
 
-<span
-  class="label inline-block px-2 py-1 rounded-full text-[10px] align-middle text-slate-700 {bgColor}"
-  >{text}
+<span class="label inline-flex rounded-full px-2 py-1 text-[10px] font-medium text-slate-700 {bgColor}">
+  {text}
 </span>
--- src/components/dad-reply/partials/Logo.svelte
+++ src/components/dad-reply/partials/Logo.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 </script>
 
-<a
-  href="#top"
-  class="thumb-swing"
->
-  <h2 class="flex items-center text-xl font-display font-bold tracking-tight">
+<a href="#top" class="thumb-swing">
+  <h2 class="font-display flex items-center text-xl font-bold tracking-tight">
     <span class="mr-2">👍</span>
     <span class="leading-none">Dad Reply</span>
-  </h2></a
->
+  </h2>
+</a>
 
 <style>
   @keyframes thumbSwing {
