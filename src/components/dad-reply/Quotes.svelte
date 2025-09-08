863e3049f7e9ec4cc5975092d9bbbf7b28a52775
Paul Mackenzie <paul@whatspauldoing.com>
2025-09-08 22:42:23 +0100

    Adding header to Quotes section. Updating URLs on quotes.json. Copy updates.

--- src/components/dad-reply/Quotes.svelte
+++ src/components/dad-reply/Quotes.svelte
@@ -3,10 +3,19 @@
   import quoteData from "@data/dad-reply/quotes.json";
 </script>
 
-<div class="grid grid-cols-3 grid-rows-3 gap-6">
-  {#each quoteData as quote}
-    <Quote {...quote}></Quote>
-  {/each}
+<div class="space-y-12">
+  <div class="flex flex-col space-y-4 text-center">
+    <h3 class="text-[32px] font-semibold text-slate-700">
+      <span class="text-4xl">✊</span>
+      Join the rebellion against the pointless.
+    </h3>
+    <p class="text-slate-500">1000’s of thumbs, 1000’s of minutes saved. Join the movement.</p>
+  </div>
+  <div class="grid grid-cols-3 grid-rows-3 gap-6">
+    {#each quoteData as quote}
+      <Quote {...quote}></Quote>
+    {/each}
+  </div>
 </div>
 
 <style>
