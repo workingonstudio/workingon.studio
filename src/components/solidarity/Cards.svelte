ce9d2d01db73fa07ebda712ce9e0732c86c8df14
Paul Mackenzie <paul@whatspauldoing.com>
2025-09-17 11:35:05 +0100

    Adding overlay to circle images. Adding boolen for OTW-overlay.

--- src/components/solidarity/Cards.svelte
+++ src/components/solidarity/Cards.svelte
@@ -6,55 +6,69 @@
       src: "https://placehold.co/64x64@2x.png",
       width: 64,
       height: 64,
+      overlay: false,
     },
     {
       src: "https://placehold.co/64x64@2x.png",
       width: 64,
       height: 64,
+      overlay: false,
     },
     {
       src: "https://placehold.co/64x64@2x.png",
       width: 64,
       height: 64,
+      overlay: false,
     },
     {
       src: "https://placehold.co/64x64@2x.png",
       width: 64,
       height: 64,
+      overlay: false,
     },
     {
-      src: "../../../../public/solidarity/me-shoe.png",
+      src: "/solidarity/me-shoe.png",
       width: 128,
       height: 128,
+      overlay: true,
     },
     {
       src: "https://placehold.co/64x64@2x.png",
       width: 64,
       height: 64,
+      overlay: false,
     },
     {
       src: "https://placehold.co/64x64@2x.png",
       width: 64,
       height: 64,
+      overlay: false,
     },
     {
       src: "https://placehold.co/64x64@2x.png",
       width: 64,
       height: 64,
+      overlay: false,
     },
     {
       src: "https://placehold.co/64x64@2x.png",
       width: 64,
       height: 64,
+      overlay: false,
     },
   ];
 </script>
 
-<div class="relative mt-32 flex w-1/3 items-start justify-center">
+<div class="relative flex h-full w-full items-start justify-center">
+  <div class="overlay absolute top-[-2px] z-10 min-h-[325px] w-full"></div>
   {#each users as user, index}
     <Card {...user} itemNumber={index} />
   {/each}
 </div>
 
 <style>
+  @reference "@styles/solidarity.css";
+  .overlay {
+    background: url("/solidarity/gradient-bg.png") center/cover no-repeat;
+  }
 </style>
--- src/components/solidarity/partials/Card.svelte
+++ src/components/solidarity/partials/Card.svelte
@@ -3,6 +3,7 @@
   export let height: number;
   export let width: number;
   export let itemNumber: number;
+  export let overlay: boolean;
 
   function getItemClasses(itemNumber: number): string {
     if (itemNumber === 4) return "center-item absolute top-32 left-1/2 z-10";
@@ -12,11 +13,13 @@
   let checkItemNumber = itemNumber === 4 ? "shadow-lg shadow-stone-900/10" : "";
   $: itemAlign = getItemClasses(itemNumber);
 
-  $: sizeClass = width === 64 ? "w-16 h-16" : width === 128 ? "w-32 h-32" : "";
+  $: sizeClass = width === 64 ? "w-16 h-16" : width === 128 ? "w-32 h-32 shadow" : "";
 </script>
 
 <div class={itemAlign}>
-  <img src="../../../../public/solidarity/otw-overlay.png" class="overlay absolute {sizeClass} rounded-full" />
+  {#if overlay}
+    <img src="/solidarity/otw-overlay.png" class="overlay absolute {sizeClass} rounded-full" />
+  {/if}
   <img {src} alt="" {width} {height} class="rounded-full border-transparent {checkItemNumber}" />
 </div>
 
@@ -50,5 +53,14 @@
   }
   .circle-item-8 {
     transform: translate(calc(-50% - 92px), -92px);
+  }
+
+  .shadow {
+    box-shadow:
+      0 83px 23px 0 rgba(105, 105, 105, 0),
+      0 53px 21px 0 rgba(105, 105, 105, 0.01),
+      0 30px 18px 0 rgba(105, 105, 105, 0.05),
+      0 13px 13px 0 rgba(105, 105, 105, 0.09),
+      0 3px 7px 0 rgba(105, 105, 105, 0.1);
   }
 </style>
