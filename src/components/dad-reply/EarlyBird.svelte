33c5dd5ce61006914c54af7bb73f31305f871dfe
Paul Mackenzie <paul@whatspauldoing.com>
2025-09-20 06:15:25 -0700

    Beginning to add fetch requests

--- src/components/dad-reply/EarlyBird.svelte
+++ src/components/dad-reply/EarlyBird.svelte
@@ -1,19 +1,51 @@
 <script lang="ts">
   import Badge from "./partials/Badge.svelte";
 
-  let isSubmitting: boolean = false;
-  let success: boolean = false;
+  import { createForm } from "felte";
+  import { validator } from "@felte/validator-zod";
+  import * as zod from "zod";
 
-  function handleSubmit() {
-    isSubmitting = true;
-    setTimeout(() => {
-      success = true;
-    }, 1000);
-  }
+  const apiKey = import.meta.env.LS_API_KEY;
+
+  const fetchConfig = {
+    method: "POST",
+    headers: {
+      content: "application/json",
+      Authorization: "Bearer" + apiKey,
+    },
+  };
+
+  let submitted = false;
+
+  const schema = zod.object({
+    email: zod.string().email().nonempty(),
+  });
+
+  const { form, isSubmitting, isValid, data } = createForm({
+    extend: validator({ schema }),
+    onSubmit: async (values) => {
+      const response = await fetch("https://api.lemonsqueezy.com/v1/customers", {
+        method: "POST",
+        headers: {
+          content: "application/json",
+          Authorization: "",
+        },
+        body: JSON.stringify({
+          data: {
+            type: "customers",
+            attributes: {
+              email: values.email,
+            },
+          },
+        }),
+      });
+      submitted = true;
+    },
+  });
 </script>
 
 <div id="earlybird" class="flex flex-col">
-  {#if !isSubmitting}
+  {#if !$isSubmitting && !submitted}
     <div class="flex flex-col items-center justify-center gap-12 md:flex-row">
       <div class="flex w-full flex-col space-y-4 md:w-md">
         <span class="cursor-default text-4xl">🪱</span>
@@ -33,15 +65,15 @@
             <li class="before:mr-2 before:content-['👍']">Access to Beta features.</li>
           </ul>
         </div>
-        <form on:submit|preventDefault={handleSubmit} class="space-y-5">
+        <form use:form class="space-y-5">
           <label class="flex" for="email">
-            <input id="email" type="email" placeholder="Email address..." class="custom-outline" />
+            <input id="email" name="email" type="email" placeholder="Email address..." class="custom-outline" />
           </label>
-          <button type="submit" class="btn solid w-full">Claim offer</button>
+          <button type="submit" class="btn solid w-full" disabled={!$isValid}>Claim offer</button>
         </form>
       </div>
     </div>
-  {:else if !success}
+  {:else if $isSubmitting}
     <div class="my-16 flex flex-col justify-center space-y-3 text-center">
       <span class="cursor-default text-4xl">🕵️‍♂️</span>
       <h3>Hold on...</h3>
@@ -49,16 +81,16 @@
       <span class="flex flex-1 items-center justify-center">
         <iconify-icon
           icon="material-symbols:progress-activity"
-          class="animate-spinl text-2xl text-slate-400"
+          class="animate-spin text-2xl text-slate-400"
         ></iconify-icon>
       </span>
     </div>
-  {:else if success && isSubmitting}
+  {:else if submitted}
     <div class="my-16 flex flex-col justify-center space-y-3 text-center">
       <span class="cursor-default text-4xl">🪿</span>
       <h3>Check your email you silly goose.</h3>
       <p>
-        Confirmation email just sent to <span class="font-mono text-slate-700">paul@workingon.studio</span>
+        Confirmation email just sent to <span class="font-mono text-slate-700">{$data.email}</span>
       </p>
       <small>
         * could be in spam amongst all those <em>other</em>
