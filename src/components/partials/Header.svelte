263303f9fe17312cb11d6ff350607142aaef96a8
Paul Mackenzie <paul@whatspauldoing.com>
2025-09-29 23:08:20 +0100

    Updating navigation with icons. Adding navigation object.

--- src/components/partials/Header.svelte
+++ src/components/partials/Header.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
   import { slide, fade } from "svelte/transition";
   import { cubicOut, cubicInOut } from "svelte/easing";
-  import timelineData from "../../data/timeline-github.json";
-  import { totals } from "../../stores/finance.js";
 
-  $: totalClass = $totals.isPositive ? "!text-green-500" : "!text-red-500";
   export let text: string;
   let headerText: string;
   let dotText: string;
-  let currentVersion = timelineData.entries.slice(-1).pop()?.version;
 
   function grabHeader(text: string) {
     let stringLength = text.length;
@@ -32,6 +28,30 @@
   }
 
   grabHeader(text);
+
+  let navItems = [
+    {
+      icon: "carbon:delivery-parcel",
+      href: "/projects/",
+      title: "projects",
+      subtitle: ".list",
+      description: "live studio projects.",
+    },
+    {
+      icon: "carbon:piggy-bank-slot",
+      href: "/finances",
+      title: "finances",
+      subtitle: ".csv",
+      description: "financials coming in/out of the studio.",
+    },
+    {
+      icon: "carbon:version",
+      href: "/progress",
+      title: "progress",
+      subtitle: ".git",
+      description: "every update, every change.",
+    },
+  ];
 </script>
 
 <div class="sticky top-0 space-y-6 bg-gray-950/80 py-8 backdrop-blur-sm">
@@ -40,9 +60,8 @@
       <!-- prettier-ignore -->
       <h1 class="font-display inline-block">{headerText}<span>{dotText}</span></h1>
     </a>
-    <button type="button">
+    <button type="button" onclick={toggleMenu}>
       <iconify-icon
-        onclick={toggleMenu}
         {icon}
         class="text-2xl text-gray-500 transition-colors duration-500 hover:cursor-pointer hover:text-gray-100 {isSpinning
           ? 'spinning'
@@ -54,39 +73,23 @@
     <nav
       class="flex flex-col items-start text-xs"
       in:slide={{ duration: 300, easing: cubicOut }}
-      out:slide={{ duration: 300, easing: cubicInOut }}
+      out:slide={{ duration: 300, easing: cubicInOut, delay: 250 }}
     >
       <ul
-        class="w-full pb-4 *:py-3"
-        in:fade={{ duration: 300, easing: cubicOut }}
+        class="flex w-full flex-col gap-4"
+        in:fade={{ duration: 300, easing: cubicOut, delay: 250 }}
         out:fade={{ duration: 300, easing: cubicInOut }}
       >
-        <li class="group">
-          <a href="/projects/">
-            <!-- prettier-ignore -->
-            <h2>projects<span>.list</span></h2>
-          </a>
-        </li>
-        <li class="group">
-          <a href="/finances">
-            <!-- prettier-ignore -->
-            <h2>finance<span>.csv</span></h2>
-            <div class="flex flex-row gap-2 {totalClass} items-center">
-              <iconify-icon icon="carbon:piggy-bank-slot" class="text-lg"></iconify-icon>
-              <span>{$totals.formatted}</span>
-            </div>
-          </a>
-        </li>
-        <li class="group">
-          <a href="/progress">
-            <!-- prettier-ignore -->
-            <h2>progress<span>.git</span></h2>
-            <div class="flex flex-row items-center gap-2 text-gray-500">
-              <iconify-icon icon="carbon:version" class="text-lg"></iconify-icon>
-              <span>{currentVersion}</span>
-            </div>
-          </a>
-        </li>
+        {#each navItems as { icon, href, title, subtitle, description }}
+          <li class="group">
+            <iconify-icon {icon} class="text-lg text-gray-500"></iconify-icon>
+            <a {href}>
+              <!-- prettier-ignore -->
+              <h2>{title}<span>{subtitle}</span></h2>
+              <p>{description}</p>
+            </a>
+          </li>
+        {/each}
       </ul>
     </nav>
   {/if}
@@ -109,9 +112,12 @@
   nav {
     ul {
       li {
-        @apply flex flex-row;
+        @apply flex flex-row items-center gap-4;
         a {
-          @apply flex w-full flex-row justify-between;
+          @apply flex w-full flex-col justify-between gap-1;
+          p {
+            @apply text-xs text-gray-500;
+          }
         }
       }
     }
