5a898a587474251a4af9fa501ea0a90e51f9448f
Paul Mackenzie <paul@whatspauldoing.com>
2025-10-16 00:35:40 +0100

    Initial setup. Adding Header

---
+++ src/components/sketch-today/Header.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<header>
+  <div class="flex flex-row gap-3">
+    <div class="flex p-1">
+      <iconify-icon icon="material-symbols:diamond-shine-outline" class="text-lg"></iconify-icon>
+    </div>
+    <div class="flex">
+      Sketch Today
+      <span>Modern Sketch Resources</span>
+    </div>
+  </div>
+</header>
+
+<style>
+  @reference "@styles/solidarity.css";
+</style>
