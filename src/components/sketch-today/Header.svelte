3a2068e81c49b4f15ca11e1f6a8f3a7879703829
Paul Mackenzie <paul@whatspauldoing.com>
2025-10-16 07:42:37 +0100

    Styling interactions.

--- src/components/sketch-today/Header.svelte
+++ src/components/sketch-today/Header.svelte
@@ -4,17 +4,24 @@
 
 <header class="flex w-full flex-row items-center justify-between">
   <div class="flex flex-row gap-3">
-    <div class="bg-main flex rounded-lg p-1">
-      <iconify-icon icon="material-symbols:diamond-shine-outline" class="text-lg text-white"></iconify-icon>
-    </div>
-    <div class="flex items-center gap-3">
+    <a class="group flex flex-row items-center gap-3" href="/">
+      <div class="logo-mark">
+        <iconify-icon icon="material-symbols:diamond-shine-outline" class="icon"></iconify-icon>
+      </div>
       <span class="font-bold">Sketch Today</span>
-      <span class="text-body text-xs font-medium">Modern Sketch Resources</span>
-    </div>
+      <span class="text-body hidden text-xs font-medium md:flex">Modern Sketch Resources</span>
+    </a>
   </div>
   <Navigation />
 </header>
 
 <style>
   @reference "@styles/sketch-today.css";
+  .logo-mark {
+    @apply bg-main flex rounded-lg p-1 group-hover:bg-stone-50;
+    .icon {
+      @apply text-lg text-white;
+      @apply group-hover:text-primary;
+    }
+  }
 </style>
--- src/components/sketch-today/partials/Navigation.svelte
+++ src/components/sketch-today/partials/Navigation.svelte
@@ -2,29 +2,25 @@
 </script>
 
 <nav class="flex flex-row items-center">
-  <ul class="text-body flex flex-row gap-6 rounded-full bg-stone-100 px-6 text-xs font-bold">
-    <li><a href="">Why?</a></li>
-    <li><a href="">News</a></li>
-    <li><a href="">Community</a></li>
+  <ul class="text-body flex flex-row gap-6 rounded-full bg-stone-100 px-6 text-xs">
+    <li><a href="/">Why?</a></li>
+    <li><a href="/">News</a></li>
+    <li><a href="/">Community</a></li>
   </ul>
-  <a class="rss px-3" href="" aria-label="Subscribe to Rss">
+  <a class="rss px-3" href="/" aria-label="Subscribe to Rss">
     <iconify-icon icon="heroicons:rss-solid" class="text-md"></iconify-icon>
   </a>
 </nav>
 
 <style>
   @reference "@styles/sketch-today.css";
-
   nav {
     a {
-      @apply hover:text-main;
+      @apply hover:text-main font-bold;
       @apply block py-3;
     }
   }
-  a {
-    @apply transition-all duration-200;
-  }
   .rss {
-    @apply text-main hover:text-orange-400;
+    @apply text-main hover:text-amber-500;
   }
 </style>
