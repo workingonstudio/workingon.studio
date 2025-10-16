13527a078e2260529939bc634dcbf05cee7fd5f2
Paul Mackenzie <paul@whatspauldoing.com>
2025-10-16 07:13:00 +0100

    Styling header. Adding Navigation

--- src/components/sketch-today/Header.svelte
+++ src/components/sketch-today/Header.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
+  import Navigation from "./partials/Navigation.svelte";
 </script>
 
-<header>
+<header class="flex w-full flex-row items-center justify-between">
   <div class="flex flex-row gap-3">
-    <div class="flex p-1">
-      <iconify-icon icon="material-symbols:diamond-shine-outline" class="text-lg"></iconify-icon>
+    <div class="bg-main flex rounded-lg p-1">
+      <iconify-icon icon="material-symbols:diamond-shine-outline" class="text-lg text-white"></iconify-icon>
     </div>
-    <div class="flex">
-      Sketch Today
-      <span>Modern Sketch Resources</span>
+    <div class="flex items-center gap-3">
+      <span class="font-bold">Sketch Today</span>
+      <span class="text-body text-xs font-medium">Modern Sketch Resources</span>
     </div>
   </div>
+  <Navigation />
 </header>
 
 <style>
-  @reference "@styles/solidarity.css";
+  @reference "@styles/sketch-today.css";
 </style>
