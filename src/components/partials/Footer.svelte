6fa7ca02a387067004e7f9bcd5621d784a433e09
Paul Mackenzie <paul@whatspauldoing.com>
2025-10-27 14:28:12 +0000

    Adding links to footer. Updating logic around typeface list.

--- src/components/partials/Footer.svelte
+++ src/components/partials/Footer.svelte
@@ -3,26 +3,14 @@
   import timelineData from "../../data/timeline-github.json";
   let date = DateTime.fromISO(timelineData.generated).toFormat("ccc',' dd LLL y TT ZZZ");
 
-  let links = [
+  export let typefaces = [
     {
-      icon: "carbon:development",
-      href: "/",
-      text: "astro + svelte",
-    },
-    {
-      icon: "carbon:data-analytics",
-      href: "https://umami.is/",
-      text: "umami",
-    },
-    {
-      icon: "carbon:text-font",
+      family: "nudica",
       href: "https://www.atipofoundry.com/fonts/nudica",
-      text: "nudica + nudica mono",
     },
     {
-      icon: "carbon:update-now",
-      href: "https://github.com/workingonstudio/workingon.studio",
-      text: date,
+      family: "nudica mono",
+      href: "https://www.atipofoundry.com/fonts/nudica-mono",
     },
   ];
 </script>
@@ -31,33 +19,57 @@
   class="mt-12 flex flex-col justify-between gap-2 border-t-1 border-slate-900 py-6 lg:flex-row lg:items-center lg:gap-0"
 >
   <!-- prettier-ignore -->
-  <div class="flex flex-col">
-    <a href="/" class="group text-primary text-md font-display -mb-1">wo<span class="flicker text-muted text-xxs">.studio</span></a>
+  <div class="flex flex-col company-info">
+    <a href="/" class="group text-primary text-md font-display">wo<span class="flicker text-muted text-xxs">.studio</span></a>
     <a href="https://find-and-update.company-information.service.gov.uk/company/16700615"><small class="uppercase text-muted text-xxs">workingonstudio ltd, no: 16700615</small></a>
   </div>
   <ul class="text-muted flex flex-row gap-x-6 gap-y-2 last:flex-wrap last:lg:flex-nowrap">
-    {#each links as { icon, href, text }}
-      <li class="flex flex-row items-center">
-        <a {href} class="text-xxs flex gap-2 uppercase">
-          <span class="h-3.5 w-3.5">
-            <iconify-icon {icon} class="text-sm"></iconify-icon>
-          </span>
-          {text}
-        </a>
-      </li>
-    {/each}
+    <li class="flex flex-row items-center gap-2">
+      <div class="flex h-3.5 w-3.5">
+        <iconify-icon icon="carbon:development" class="text-sm"></iconify-icon>
+      </div>
+      <a href="https://astro.build/">astro</a>
+      <div class="relative -top-[1px]">+</div>
+      <a href="https://svelte.dev/">Svelte</a>
+    </li>
+    <li class="flex flex-row items-center gap-2">
+      <div class="flex h-3.5 w-3.5">
+        <iconify-icon icon="carbon:data-analytics" class="text-sm"></iconify-icon>
+      </div>
+      <a href="https://umami.is/" class="">Umami</a>
+    </li>
+    <li class="flex flex-row items-center gap-2">
+      <div class="flex h-3.5 w-3.5">
+        <iconify-icon icon="carbon:text-font" class="text-sm"></iconify-icon>
+      </div>
+      {#each typefaces as { family, href }, index}
+        <a {href} class="">{family}</a>
+        {#if index < typefaces.length - 1}
+          <div class="relative -top-[1px]">+</div>
+        {/if}
+      {/each}
+    </li>
+    <li class="flex flex-row items-center gap-2">
+      <div class="flex h-3.5 w-3.5">
+        <iconify-icon icon="carbon:update-now" class="text-sm"></iconify-icon>
+      </div>
+      <a href="https://github.com/workingonstudio/workingon.studio/commits/main/">{date}</a>
+    </li>
   </ul>
 </footer>
 
 <style>
   @reference "@styles/main.css";
-  footer {
-    a,
-    small {
-      @apply hover:text-primary transition-all delay-100;
+  footer ul {
+    li {
+      a,
+      small {
+        @apply hover:text-primary;
+        @apply text-xxs flex uppercase;
+      }
     }
-    .flicker {
-      @apply text-shadow-glow motion-safe:animate-flicker text-yellow-300;
-    }
+  }
+  .flicker {
+    @apply text-shadow-glow motion-safe:animate-flicker text-yellow-300;
   }
 </style>
