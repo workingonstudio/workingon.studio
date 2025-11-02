ae7014ee0cb516527caad1f4ebaf1795a546eafa
Paul Mackenzie <paul@whatspauldoing.com>
2025-11-02 22:01:43 +0000

    Updating homepage copy. Adding text-pretty to paragraphs. Bumping Dad Reply version on Project list.

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -9,14 +9,15 @@
   </h1>
   <div class="flex max-w-xl flex-col gap-12">
     <p>
-      They're not customers, they’re not clients, or partners, they're people. Messy humans, with issues far greater
-      than &#x0022I best get this report done&#x0022.
+      They're not users, they're not customers, they’re not clients, or partners, they're people. Messy humans, with issues far greater
+      than &#x0022I'd best get this report done&#x0022.
     </p>
     <p>
       They’re humans that don’t need to be talked to like idiots. The world isn’t all sunshine and rainbows. They don’t
       need to be manipulated, told that if they just buy &#x0022this thing&#x0022 their life will be complete.
     </p>
-    <p>This is just a ride. And I’d much rather make it the best ride, than trick them into thinking it is.</p>
+    <p>This is just a ride. And I’d much rather make it the best ride than trick them into thinking it is.</p>
+    <p>So I'm building products that reflect that. Cultural mirrors. Things that make the invisible visible. Design that centers people, not conversion rates.</p>
     <p>
       This game is broken.
       <br />
