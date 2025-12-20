fd339d76c277a47e50eff80e93335c81fb7d1aaf
Paul Mackenzie <paul@whatspauldoing.com>
2025-12-20 09:50:25 +0000

    Adding intial changelogs. Adding icons for hero. Altering selected icon in plugin. Updating format for changelogs

--- src/components/glyph-palette/Changelog.svelte
+++ src/components/glyph-palette/Changelog.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
   import Icon from "./Icon.svelte";
+  import Badge from "./Badge.svelte";
+  import { DateTime } from "luxon";
 
   export let productChangelogs;
 </script>
 
-<section class="flex flex-row">
+<section class=" my-6 flex flex-col justify-between md:flex-row">
   <div class="flex flex-col gap-6">
     <Icon icon="update-rounded" />
     <h1>Changelog</h1>
-    <p class="text-xl">All updates.</p>
+    <p class="text-xl">Updates. Fixes. Additions. Improvements.</p>
   </div>
+  <ul class="flex w-1/2 flex-col gap-8">
+    {#each productChangelogs as { date, title, description, version, items }}
+      <li class="flex w-full flex-col">
+        <div class="flex flex-row items-center justify-between">
+          <Badge label={"v" + version.major + "." + version.minor + "." + version.patch} />
+          <time datetime={date}>{DateTime.fromJSDate(date).toLocaleString({ month: "short", day: "numeric" })}</time>
+        </div>
+      </li>
+    {/each}
+  </ul>
 </section>
 
 <style>
   @reference "@styles/glyph-palette.css";
+  time {
+    @apply text-muted text-xxs uppercase;
+  }
 </style>
--- src/components/glyph-palette/Hero.svelte
+++ src/components/glyph-palette/Hero.svelte
@@ -1,12 +1,13 @@
 <script>
-  import Form from "./Form.svelte";
   import Plugin from "./Plugin.svelte";
+  import Icon from "./Icon.svelte";
 </script>
 
 <div class="my-6 flex flex-col items-center justify-between gap-6 md:my-12 md:gap-12 lg:flex-row">
   <div class="flex flex-col gap-16">
     <section class="flex w-full flex-col items-center gap-12 lg:w-2xl lg:items-start">
       <div class="hero flex flex-col gap-6 text-center md:w-2/3 lg:w-full lg:text-left">
+        <Icon icon="diamond-outline" />
         <h1>A modern icon plugin for Sketch.</h1>
         <p class="w-full text-xl lg:w-xl">
           Bring 100’s of modern icon* sets directly to into your .sketch files, with full control over size, stroke, and
