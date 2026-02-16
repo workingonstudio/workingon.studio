86f494139c171fce339830ccf1e218af9ffb91ec
Paul Mackenzie <paul@whatspauldoing.com>
2026-02-16 23:29:29 +0000

    Adding basic structure. Sorting theme. Adding basic layout.

---
+++ src/components/tiny-wave/Header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-row items-center gap-2 text-base">
+  <iconify-icon icon="ph:waveform" class="text-primary size-6 text-2xl"></iconify-icon>
+  tinywave
+</div>
+
+<style>
+</style>
---
+++ src/components/tiny-wave/ToggleMute.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let icon = $state("ph:microphone-slash-fill");
+
+  function toggleMute() {
+    icon = icon === "ph:microphone-slash-fill" ? "ph:microphone-fill" : "ph:microphone-slash-fill";
+  }
+</script>
+
+<button type="button" onclick={toggleMute} aria-label="mute" class="btn flex size-8 items-center justify-center">
+  <iconify-icon {icon} class="text-body size-4 text-base"></iconify-icon>
+</button>
+
+<style>
+  @reference "@styles/tiny-wave.css";
+</style>
