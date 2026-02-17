fa8290451f81e70e3eadb0e01999ca14b52dbf51
Paul Mackenzie <paul@whatspauldoing.com>
2026-02-17 09:20:50 +0000

    Adding footer

---
+++ src/components/tiny-wave/Footer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Logo from "@components/partials/Logo.svelte";
+</script>
+
+<footer class="flex flex-row justify-between">
+  <a href="/" class="text-xxs text-muted flex flex-row items-center gap-2 font-bold">
+    <Logo width={19} />
+    workingonstudio ltd, no: 16700615
+  </a>
+</footer>
+
+<style>
+</style>
