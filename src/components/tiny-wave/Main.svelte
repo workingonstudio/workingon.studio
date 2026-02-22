ab426fe34c5b8bbd8688eb826cc2a064e0a06130
Paul Mackenzie <paul@whatspauldoing.com>
2026-02-22 12:48:21 +0000

    Moving permission logic to main.svelte

--- src/components/tiny-wave/Main.svelte
+++ src/components/tiny-wave/Main.svelte
@@ -4,13 +4,26 @@
   import Permissions from "./partials/Permissions.svelte";
   import RecordPanel from "./RecordPanel.svelte";
 
-  import { slide } from "svelte/transition";
+  import { slide, fade } from "svelte/transition";
 
   import { recorder } from "@stores/tiny-wave/recorder.svelte";
+
+  let checkedPermission = $state(false);
+
+  $effect(() => {
+    navigator.permissions.query({ name: "microphone" as PermissionName }).then((result) => {
+      if (result.state === "granted") {
+        recorder.handleMicClick();
+      }
+      checkedPermission = true;
+    });
+  });
 </script>
 
 <div class="flex w-full flex-col gap-6 lg:w-2xl">
-  <Permissions />
+  {#if checkedPermission && recorder.permissionState === "idle"}
+    <Permissions />
+  {/if}
   <div class="flex flex-col rounded-xl border border-gray-300 bg-white p-6 shadow-lg/5 ring ring-gray-100">
     <div class="flex flex-row items-center justify-between">
       <Header />
