56d74c334f259a89c245d982dcd9d269de970d4d
Paul Mackenzie <paul@whatspauldoing.com>
2026-02-22 12:35:11 +0000

    Adding animation when user clicks unmute.

--- src/components/tiny-wave/Main.svelte
+++ src/components/tiny-wave/Main.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-  import Footer from "./partials/Footer.svelte";
   import Header from "./partials/Header.svelte";
+  import Footer from "./partials/Footer.svelte";
   import Permissions from "./partials/Permissions.svelte";
   import RecordPanel from "./RecordPanel.svelte";
+
+  import { slide } from "svelte/transition";
+
   import { recorder } from "@stores/tiny-wave/recorder.svelte";
 </script>
 
 <div class="flex w-full flex-col gap-6 lg:w-2xl">
   <Permissions />
-  <div class="flex flex-col gap-6 rounded-xl border border-gray-300 bg-white p-6 shadow-lg/5 ring ring-gray-100">
+  <div class="flex flex-col rounded-xl border border-gray-300 bg-white p-6 shadow-lg/5 ring ring-gray-100">
     <div class="flex flex-row items-center justify-between">
       <Header />
     </div>
     {#if !recorder.isMuted}
-      <RecordPanel />
+      <div transition:slide class="pt-6">
+        <RecordPanel />
+      </div>
     {/if}
   </div>
   <Footer />
