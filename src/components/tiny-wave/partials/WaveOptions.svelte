13f4f151214a7948a5edb838632a50d247fa2d22
Paul Mackenzie <paul@whatspauldoing.com>
2026-02-25 20:04:25 +0000

    Fixing spelling error in Tinywave footer. Added aria-labels to option buttons.

--- src/components/tiny-wave/partials/WaveOptions.svelte
+++ src/components/tiny-wave/partials/WaveOptions.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { recorder } from "@stores/tiny-wave/recorder.svelte";
   import ToolTip from "./ToolTip.svelte";
+  import { recorder } from "@stores/tiny-wave/recorder.svelte";
 </script>
 
 <div class="flex flex-row justify-center gap-3">
@@ -8,6 +8,7 @@
     <button
       type="button"
       onclick={() => recorder.downloadSvg()}
+      aria-label="Download"
       class="btn text-muted hover:text-body flex size-8 items-center justify-center"
     >
       <iconify-icon icon="ph:download-simple-bold" class="text-base"></iconify-icon>
@@ -18,6 +19,7 @@
     <button
       type="button"
       onclick={() => recorder.copyToClipboard()}
+      aria-label="Copy to Clipboard"
       class="btn text-muted hover:text-body flex size-8 items-center justify-center"
     >
       <iconify-icon icon="ph:copy-bold" class="text-base"></iconify-icon>
@@ -28,6 +30,7 @@
     <button
       type="button"
       onclick={() => recorder.reset()}
+      aria-label="Delete"
       class="btn text-muted hover:text-primary flex size-8 items-center justify-center"
     >
       <iconify-icon icon="ph:trash-bold" class="text-base"></iconify-icon>
