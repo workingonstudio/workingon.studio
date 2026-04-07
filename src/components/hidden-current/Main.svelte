d9b4c3745e38444bc08d72798db2ff9de3ce73d9
Paul Mackenzie <paul@whatspauldoing.com>
2026-04-07 10:17:20 +0100

    Hiding signup form

--- src/components/hidden-current/Main.svelte
+++ src/components/hidden-current/Main.svelte
@@ -9,7 +9,7 @@
     <Logo />
     <div class="flex flex-col gap-6 text-center text-pretty">
       <h1>Your prompts really are costing the earth.</h1>
-      <p class="text-xl">Track how much water and power your prompts are using.*</p>
+      <p class="text-md md:text-xl">Track how much water and power your prompts are using.*</p>
       <small class="flex flex-row justify-center gap-1">
         * only compatible with <div class="flex flex-row items-center gap-1">
           <iconify-icon icon="fa7-brands:claude" class="size-3 text-sm text-[#C15F3C]"></iconify-icon>
@@ -17,18 +17,23 @@
         </div>
       </small>
     </div>
-    <div class="flex flex-col gap-6 md:flex-row">
-      <a
-        href="/"
-        class="hover:border-primary hover:text-primary flex flex-row items-center gap-4.5 rounded-full border-3 border-transparent bg-neutral-900 px-5 py-4 text-base font-bold text-white transition-colors duration-300 hover:border-3 hover:bg-white"
+    <!-- <form action="" class="flex flex-row gap-3 rounded-full border border-neutral-300 p-4 shadow-lg">
+      <label>
+        <input type="text" class="w-xs rounded-full border border-neutral-300 px-4 py-2" />
+      </label>
+      <button
+        type="button"
+        class="bg-primary hover:border-primary hover:text-primary cursor-pointer rounded-full px-4 py-2.5 text-sm font-semibold text-white hover:bg-white"
       >
+        Submit
+      </button>
+    </form> -->
+    <div class="flex flex-col gap-6 md:flex md:flex-row">
+      <a href="/" class="downloadBtn">
         <iconify-icon icon="fa7-brands:chrome" class="size-6 text-2xl"></iconify-icon>
         Add to Chrome
       </a>
-      <a
-        href="/"
-        class="hover:border-primary hover:text-primary flex flex-row items-center gap-4.5 rounded-full border-3 border-transparent bg-neutral-900 px-5 py-4 text-base font-bold text-white transition-colors duration-300 hover:border-3 hover:bg-white"
-      >
+      <a href="/" class="downloadBtn">
         <iconify-icon icon="fa7-brands:firefox" class="size-6 text-2xl"></iconify-icon>
         Add to Firefox
       </a>
