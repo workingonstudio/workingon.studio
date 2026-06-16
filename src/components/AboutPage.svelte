ba5be02be05042139043422f9279b17dca0d4781
Paul Mackenzie <paul@workingon.studio>
2026-06-16 11:05:25 +0100

    Update availability message in AboutPage

--- src/components/AboutPage.svelte
+++ src/components/AboutPage.svelte
@@ -36,7 +36,7 @@
     </p>
     <!-- prettier-ignore -->
     <p>
-      Available now. Contract or full-time. If you ship more than you meet, we'll get on, 
+      Available now. Contract or full-time. If you ship more than you meet, 
       <a
         href="/contact"
         class="underline hover:no-underline"
