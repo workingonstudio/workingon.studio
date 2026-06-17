5dbd5b85a81742a858a97df87160936fa086d424
Paul Mackenzie <paul@whatspauldoing.com>
2026-06-17 17:20:04 +0100

    Fixing odd character on about page.

--- src/components/AboutPage.svelte
+++ src/components/AboutPage.svelte
@@ -36,11 +36,7 @@
     </p>
     <!-- prettier-ignore -->
     <p>
-      Available now. Contract or full-time. If you ship more than you meet, 
-§      <a
-        href="/contact"
-        class="underline hover:no-underline"
-      >we'll get on</a>.
+      Available now. Contract or full-time. If you ship more than you meet, <a href="/contact" class="underline hover:no-underline">we'll get on</a>.
     </p>
   </ContentPanel>
 
