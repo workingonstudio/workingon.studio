b65172ae459193c3702b7091e82654a6466449b8
Paul Mackenzie <paul@whatspauldoing.com>
2026-08-14 18:19:46 +0100

    Removing GlyphPalette components. Updating Project calculations.

--- src/components/glyph-palette/Main.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import Hero from "./Hero.svelte";
-  import Features from "./Features.svelte";
-  import Roadmap from "./Roadmap.svelte";
-</script>
-
-<Hero />
-<Features />
-<Roadmap />
---
+++ src/components/partials/DaysCounter.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { untrack } from "svelte";
+
+  let { launchDate, initialDays }: { launchDate: string; initialDays: number } = $props();
+
+  let days = $state(untrack(() => initialDays));
+
+  $effect(() => {
+    days = Math.floor((Date.now() - new Date(launchDate).getTime()) / (1000 * 60 * 60 * 24));
+  });
+</script>
+
+{days}
